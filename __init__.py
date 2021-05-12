e7cc504f156c2263d16dcf313481558c71f4c694
luc wastiaux <luc.wastiaux@airpost.net>
2021-05-12 21:19:35 +0800

    working airtable record listing

--- __init__.py
+++ __init__.py
@@ -1,32 +1,53 @@
-# import the main window object (mw) from aqt
-from aqt import mw
-# import the "show info" tool from utils.py
-from aqt.utils import showInfo, qconnect
-# import all of the Qt GUI library
-from aqt.qt import *
+import aqt
+# from aqt.utils import showInfo, qconnect
+# from aqt.qt import *
+import requests
 
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
 
 def queryStatsFunction() -> None:
-    # get the number of cards in the current collection, which is stored in
-    # the main window
+
+    # retrieve config
+    # =====================
+    config = aqt.mw.addonManager.getConfig(__name__)
+    airtable_url_base = config['airtable_url_base']
+    airtable_view = config['airtable_view']
+    airtable_api_key = config['airtable_api_key']
+
+
+    # list airtable records
+    # =====================
+    url_complete = f'{airtable_url_base}?view={airtable_view}'
+
+    print(f'querying: {url_complete}')
+    response = requests.get(url_complete, headers={'Authorization': f'Bearer {airtable_api_key}'})
+    if response.status_code != 200:
+        error_message = f'received error while querying {url_complete}: {response.content} status code: {response.status_code}'
+        aqt.utils.showError(error_message)
+    
+    # we need to get the record id of airtable records, so we can update them
+    record_id_map = {}
+    data = response.json()
+    for record in data['records']:
+        record_id_map[record['fields']['Name']] = record['id']
+
+    print(record_id_map)
+    
 
     results = []
 
     for flag_id in range(0, 4):
-        id_list = mw.col.find_cards(f'flag:{flag_id}')
+        id_list = aqt.mw.col.find_cards(f'flag:{flag_id}')
         if len(id_list) > 0:
             results.append(f'flag {flag_id}: {len(id_list)} cards')
 
     # insert API call to Todoist here
 
     # show a message box
-    showInfo('\n'.join(results))
+    aqt.utils.showInfo('\n'.join(results))
 
 # create a new menu item, "test"
-action = QAction("Query Stats", mw)
+action = aqt.qt.QAction("Query Stats", aqt.mw)
 # set it to call testFunction when it's clicked
-qconnect(action.triggered, queryStatsFunction)
+aqt.utils.qconnect(action.triggered, queryStatsFunction)
 # and add it to the tools menu
-mw.form.menuTools.addAction(action)
+aqt.mw.form.menuTools.addAction(action)
